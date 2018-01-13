03072d0445dc9042d5acb45f4eebcdda5c94de72
TB Schardl <neboat@mit.edu>
2018-01-13 05:39:50 +0000

    Bug fixes for initial rebase onto version 6.

--- lib/Transforms/Scalar/SCCP.cpp
+++ lib/Transforms/Scalar/SCCP.cpp
@@ -709,9 +709,7 @@ void SCCPSolver::getFeasibleSuccessors(TerminatorInst &TI,
     return;
   }
 
-  if (isa<DetachInst>(&TI) ||
-      isa<ReattachInst>(&TI) ||
-      isa<SyncInst>(&TI)) {
+  if (isa<DetachInst>(&TI) || isa<ReattachInst>(&TI) || isa<SyncInst>(&TI)) {
     // All destinations are executable.
     Succs.assign(TI.getNumSuccessors(), true);
     return;
